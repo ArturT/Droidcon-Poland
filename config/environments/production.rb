8df0a416bdd2faec4d826fbb467fce19caf2db65
ArturT <arturtrzop@gmail.com>
2014-07-19 19:10:18 +0200

    Remove redirect middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,4 @@ DroidconPoland::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # redirect to www.krakdroid.pl
-  config.middleware.use "Krakdroid"
 end
