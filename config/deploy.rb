5942d0c43d15f283f723ab74c1fa197d2a3b2bc6
ArturT <arturtrzop@gmail.com>
2014-07-21 23:57:05 +0200

    Update stop

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,8 +53,8 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      execute "sudo /etc/init.d/unicorn-droidcon-poland stop"
-      execute "sudo /etc/init.d/unicorn-droidcon-poland start"
+      execute "/etc/init.d/unicorn-droidcon-poland stop"
+      execute "/etc/init.d/unicorn-droidcon-poland start"
     end
   end
 
