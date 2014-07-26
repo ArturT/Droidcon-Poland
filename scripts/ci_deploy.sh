4cbb9d183fca88d3af7906ed71bdbec66ed589d2
ArturT <arturtrzop@gmail.com>
2014-07-26 16:22:40 +0200

    update ci_deploy.sh

--- scripts/ci_deploy.sh
+++ scripts/ci_deploy.sh
@@ -1,11 +1,9 @@
 if [ "$BRANCH" = "master" ]; then
   echo "deploying master to staging"
   #cap staging deploy
-  #cap staging deploy:migrate
 elif [ "$BRANCH" = "production" ]; then
-  echo "deploying production"
+  echo "deploying to production"
   cap production deploy
-  #cap production deploy:migrate
 else
   echo "No deployment for branch: $BRANCH"
 fi
