67b2703259037b4547cf234236cb8f1efa8fa25d
ArturT <arturtrzop@gmail.com>
2014-07-26 14:47:59 +0200

    fix wrong type in migration

--- db/migrate/20140726124106_remove_fields_from_schedules.rb
+++ db/migrate/20140726124106_remove_fields_from_schedules.rb
@@ -2,7 +2,7 @@ class RemoveFieldsFromSchedules < ActiveRecord::Migration
   def change
     remove_column :schedules, :topic_pl, :string, null: true
     remove_column :schedules, :topic_en, :string, null: true
-    remove_column :schedules, :description_pl, :string, null: true
-    remove_column :schedules, :description_en, :string, null: true
+    remove_column :schedules, :description_pl, :text, null: true
+    remove_column :schedules, :description_en, :text, null: true
   end
 end
