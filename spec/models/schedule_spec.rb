4afa7f2e141b8428859d4d84e8efe7b335e45ed8
ArturT <arturtrzop@gmail.com>
2014-07-26 15:05:07 +0200

    fix schedule spec

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,5 @@
 describe Schedule do
   it { should belong_to(:speaker) }
   it { should belong_to(:room) }
-  it { should validate_presence_of(:room_id) }
+  it { should validate_presence_of(:room) }
 end
