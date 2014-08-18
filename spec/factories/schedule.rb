e217676833036cf16497325ab826bad04cb79b89
ArturT <arturtrzop@gmail.com>
2014-08-18 23:53:47 +0200

    Fix schedule factory failing at the end of the day

--- spec/factories/schedule.rb
+++ spec/factories/schedule.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :schedule do
     sequence(:topic) { |n| "Topic #{n}" }
     sequence(:description) { |n| "Description #{n}" }
-    start_time DateTime.now
-    end_time DateTime.now + 1.hour
+    start_time DateTime.now.beginning_of_day
+    end_time DateTime.now.beginning_of_day + 1.hour
     speaker
     room
   end
